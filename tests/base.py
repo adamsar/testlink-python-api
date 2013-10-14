8435f2cabf03ebe9082d659129f53e956c69cae7
adamsar <adamsar@gmail.com>
2013-10-14 22:47:00 +0900

    Test clean

--- tests/base.py
+++ tests/base.py
@@ -4,6 +4,5 @@ from testlink import TestLinkClient
 class TestLinkTest(TestCase):
 
     def setUp(self):
-        self.api = TestLinkClient(url="http://bptest.ubicast.com/lib/api/xmlrpc/v1/xmlrpc.php",
-                                  key="0b7baeeea2c301708a8f39f362dd0ffe")
+        self.api = TestLinkClient()
         
